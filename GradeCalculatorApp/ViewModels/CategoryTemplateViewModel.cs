9fd498f4a4eecfea53bbeb0f629b8e68606a0421
HudsonEicke <123416403+HudsonEicke@users.noreply.github.com>
2025-07-08 15:02:04 -0400

    More work on the course template builder UI

---
+++ GradeCalculatorApp/ViewModels/CategoryTemplateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using GradeCalculatorLibrary;
+
+
+namespace GradeCalculatorApp.ViewModels
+{
+    public partial class CategoryTemplateViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string? _name;
+
+        [ObservableProperty]
+        private string? _weight;
+
+        [ObservableProperty]
+        private string? _assignmentCount;
+
+        [ObservableProperty]
+        private bool _hasDrops;
+
+        [ObservableProperty]
+        private string? _dropCount;
+
+        [ObservableProperty]
+        private string? _difficulty;
+    }
+}
---
+++ GradeCalculatorApp/ViewModels/CourseTemplateBuilderViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace GradeCalculatorApp.ViewModels
+{
+    public partial class CourseTemplateBuilderViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string? _courseName;
+
+        //add letter grade stuff later
+
+        public ObservableCollection<CategoryTemplateViewModel> Categories { get; } = new ObservableCollection<CategoryTemplateViewModel>();
+
+        public CourseTemplateBuilderViewModel()
+        {
+            Categories.Add(new CategoryTemplateViewModel());
+            Categories.Add(new CategoryTemplateViewModel());
+            Categories.Add(new CategoryTemplateViewModel());
+            Categories.Add(new CategoryTemplateViewModel());
+            Categories[0].Name = "Test 1";
+            Categories[1].Name = "Test 2";
+            Categories[2].Name = "Test 3";
+            Categories[3].Name = "Test 4";
+        }
+
+    }
+}
