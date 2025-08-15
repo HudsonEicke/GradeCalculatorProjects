8090e1aedd4f80a51599cb74125c271f087fdadc
HudsonEicke <123416403+HudsonEicke@users.noreply.github.com>
2025-08-15 17:16:51 -0400

    Started work on view models for calculator

---
+++ GradeCalculatorApp/ViewModels/AssignmentViewModel.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculatorApp.ViewModels
+{
+    public partial class AssignmentViewModel : ObservableValidator
+    {
+        [ObservableProperty]
+        private int _assignmentNum;
+
+        [ObservableProperty]
+        [CustomValidation(typeof(AssignmentViewModel), nameof(ValidateStringGrade))]
+        private string? _stringGrade;
+
+        public double? Grade { get; private set; }
+
+        public event EventHandler<int>? GradeChanged;
+
+        public AssignmentViewModel(int assignmentNum)
+        {
+            _assignmentNum = assignmentNum;
+        }
+
+        partial void OnStringGradeChanged(string? oldValue, string? newValue)
+        {
+            ValidateProperty(newValue, nameof(StringGrade));
+
+            if(!HasErrors)
+            {
+                if(string.IsNullOrEmpty(newValue))
+                {
+                    Grade = null;
+                }
+                else
+                {
+                    Grade = double.Parse(newValue);
+                }
+
+                GradeChanged?.Invoke(this, AssignmentNum);
+            }
+        }
+
+        public static ValidationResult ValidateStringGrade(string stringGrade, ValidationContext context)
+        {
+            if(!string.IsNullOrEmpty(stringGrade) && !double.TryParse(stringGrade, out double grade))
+            {
+                return new("Invalid value");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ GradeCalculatorApp/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using GradeCalculatorLibrary;
+
+namespace GradeCalculatorApp.ViewModels
+{
+    public partial class CategoryViewModel : ViewModelBase
+    {
+        private Category _category;
+        public ObservableCollection<AssignmentViewModel> Assignments { get; } = new ObservableCollection<AssignmentViewModel>();
+
+        [ObservableProperty]
+        private double _grade;
+
+        public CategoryViewModel(Category category)
+        {
+            _category = category;
+
+            for (int i = 0; i < category.AssignmentCount; i++)
+            {
+                Assignments.Add(new AssignmentViewModel(i));
+                Assignments[i].GradeChanged += AssignmentGradeChange;
+            }
+        }
+
+        private void AssignmentGradeChange(object? sender, int assignmentNum)
+        {
+            if(Assignments[assignmentNum].Grade == null)
+            {
+                _category.ResetScore(assignmentNum);
+            }
+            else
+            {
+                _category.SetScore(assignmentNum, (double)Assignments[assignmentNum].Grade);
+            }
+
+            Grade = _category.ObtainedScore;
+        }
+    }
+}
