f8ae639ebcf2bc7fa29947f2751a80b63b6b26cd
HudsonEicke <123416403+HudsonEicke@users.noreply.github.com>
2025-08-18 11:46:38 -0400

    Continued work on view models for calculator

--- GradeCalculatorApp/ViewModels/CategoryViewModel.cs
+++ GradeCalculatorApp/ViewModels/CategoryViewModel.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using GradeCalculatorLibrary;
 
+//maybe add a refresh button
+
 namespace GradeCalculatorApp.ViewModels
 {
     public partial class CategoryViewModel : ViewModelBase
@@ -17,6 +19,14 @@ namespace GradeCalculatorApp.ViewModels
         [ObservableProperty]
         private double _grade;
 
+        public double MaxWeight { get => _category.Weight; }
+
+        [ObservableProperty]
+        private string _name;
+
+        //used for displaying assignments with nums next to them ex: hw1 hw2 hw3 ...
+        public string AssignmentName {  get => _category.Name; }
+
         public CategoryViewModel(Category category)
         {
             _category = category;
@@ -26,6 +36,24 @@ namespace GradeCalculatorApp.ViewModels
                 Assignments.Add(new AssignmentViewModel(i));
                 Assignments[i].GradeChanged += AssignmentGradeChange;
             }
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            nameBuilder.Append(category.Name);
+
+            if (category.HasDrops)
+            {
+                nameBuilder.Append($" ({category.DropCount} drop");
+
+                if (category.DropCount > 1)
+                {
+                    nameBuilder.Append('s');
+                }
+
+                nameBuilder.Append(')');
+            }
+
+            Name = nameBuilder.ToString();
         }
 
         private void AssignmentGradeChange(object? sender, int assignmentNum)
